e75154855a72f46ef25c010c5ee080952e873f94
stevie <veiru.stevie@gmail.com>
2016-06-22 22:31:01 +1200

    'debug'

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -14,7 +14,7 @@
 		<div class="banner-box">
 			<p>Hi, I am Stevie. This is my personal portflio. It's a Chatroom which was built by Laravel 5.2 & Nodejs. If you enjoy it. You can visit the source code in my github:&nbsp<a target="_BLANK" href="https://github.com/stevielu/webchat">https://github.com/stevielu/webchat</a></p>
 			<p>No Accouts? </p>
-			<p style="color:cadetblue"><strong>Dmeo Account:visitor</stong></p>
+			<p style="color:cadetblue"><strong>Dmeo Account:visitor@demo.com</stong></p>
 			<p style="color:cadetblue"><strong>Password:visitor123</strong></p>
 			<button type="button" id='create-page'>Create Account</button>
 		</div>
