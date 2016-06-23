c490c4f1e60f5e0edad9fbf09115f1e0b433795a
stevie <veiru.stevie@gmail.com>
2016-06-23 12:16:21 +1200

    'debug'

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -53,6 +53,9 @@ class ChatController extends Controller
 
 	    $currentfocus = 'sidebar_chat';
 	    //$contents = $this->getChatInfoNow('1','chat-channel');
+	    $now = new DateTime();
+$now->setTimezone(new DateTimeZone('America/Los_Angeles'));
+var_dump($now->format('g:i A'));
 	    return view('layouts/chatroom', compact('username','chatroom','currentfocus'));
 	}
 
