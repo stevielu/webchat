3d78c59f50d3fff7e80ffbf13f34fc43d5cc927c
stevie <veiru.stevie@gmail.com>
2016-06-30 15:37:39 +1200

    unbind issue

--- public/js/app.js
+++ public/js/app.js
@@ -210,8 +210,8 @@ function loadingContents(data,history){
     }
     else{
         if(data['empty'] == 'last recordes'){
-            var $ret = $(window).unbind('mousewheel');
-            console.log($ret);
+            $(window).off('mousewheel');
+            //console.log($ret);
             $('.reminderinfo-sm').fadeIn();
             $('.reminderinfo-icon').fadeOut();
             $('.reminderinfo-sm').html('<p class="date">Last Recordes</p>');
