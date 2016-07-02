7b17fc41c02ea0a6f3dbfb2339d32dea637bd08e
stevie <veiru.stevie@gmail.com>
2016-07-02 21:59:28 +1200

    pull box

--- resources/views/layouts/visitorlist.blade.php
+++ resources/views/layouts/visitorlist.blade.php
@@ -2,6 +2,13 @@
 </div>
 <div class='side-pull-box'>
 	<div class='side-pull-wrap'>
-		<span class="fa fa-angle-double-right" aria-hidden="true"></span>
+		<span id='sidepull-btn' class="fa fa-angle-double-right" aria-hidden="true"></span>
 	</div>
 </div>
+<script>
+$('.side-pull-wrap').click(function () {
+	$('.visitor-box').animate({width: 'toggle'});
+	$('#sidepull-btn').toggleClass("fa-angle-double-right fa-angle-double-left");
+});
+
+</script>
