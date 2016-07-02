9844358a12fedb304e9596b64824bf0e0e1378d0
stevie <veiru.stevie@gmail.com>
2016-07-02 23:04:31 +1200

    pull box

--- resources/views/layouts/visitorlist.blade.php
+++ resources/views/layouts/visitorlist.blade.php
@@ -6,16 +6,16 @@
 	</div>
 </div>
 <script>
-$('.side-pull-wrap').click(function () {
-	if($(this).css("margin-left") == "0")
+$('.side-pull-box').click(function () {
+	if($(this).css("margin-left") == "0px")
     {
         $('.visitor-box').animate({"margin-left": '0%'});
-        $('.side-pull-wrap').animate({"margin-left": '10%'});
+        $('.side-pull-box').animate({"margin-left": '10%'});
     }
     else
     {
         $('.visitor-box').animate({"margin-left": '-10%'});
-        $('.side-pull-wrap').animate({"margin-left": '0%'});
+        $('.side-pull-box').animate({"margin-left": '0px'});
     }
 	$('#sidepull-btn').toggleClass(" fa-angle-double-left fa-angle-double-right");
 });
