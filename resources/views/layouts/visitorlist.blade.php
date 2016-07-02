5d6646381a658c20d036a470d821ab724d5a2ed0
stevie <veiru.stevie@gmail.com>
2016-07-02 22:30:18 +1200

    pull box

--- resources/views/layouts/visitorlist.blade.php
+++ resources/views/layouts/visitorlist.blade.php
@@ -9,7 +9,7 @@
 $('.side-pull-wrap').click(function () {
 	$('.visitor-box').toggle('slide', {
             direction: 'left'
-        }, 1000);
+        }, 1000,function(){$(this).css('display','inline-block');});
 	$('#sidepull-btn').toggleClass(" fa-angle-double-left fa-angle-double-right");
 });
 
