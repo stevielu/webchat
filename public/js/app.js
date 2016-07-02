575946a202cb907c23cb9b54057e29125c380c30
stevie <veiru.stevie@gmail.com>
2016-07-03 00:57:20 +1200

    3131

--- public/js/app.js
+++ public/js/app.js
@@ -211,7 +211,7 @@ function loadingContents(data,history){
     for (var key in data.visitorlist) {
         var path = imageExists(data.visitorlist[key]);
         var html = '<a class="viewprofile" link="viewprofile/'+data.visitorlist[key]+'" href="#" data-toggle="modal" data-target="#viewprofile">';
-        html += '<img class="media-object img-circle"  alt="64x64" src="'+path+'" data-holder-rendered="true" style="width: 50px; height: 50px;"></a>';
+        html += '<img class="img-circle"  alt="64x64" src="'+path+'" data-holder-rendered="true" style="width: 50px; height: 50px;"></a>';
         html += '<p>'+data.visitorlist[key]+'</p>';
         $('#visitor-list').append('<li>'+html+'</li>');
     }
