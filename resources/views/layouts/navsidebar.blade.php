a415552814199234d2e9e809eae46af379649201
stevie <stevie@tecworldnz.com>
2016-07-12 16:43:27 +1200

    debug

--- resources/views/layouts/navsidebar.blade.php
+++ resources/views/layouts/navsidebar.blade.php
@@ -21,9 +21,9 @@
 	<div class="col-md-10 navbar-list no-padding">
 	@if($currentfocus == 'sidebar_userdashboard')
     	@include('layouts.usersidebar')
-	@else if($currentfocus == 'sidebar_chat')
+	@elseif($currentfocus == 'sidebar_chat')
 		@include('layouts.mychannel')
-	@else if($currentfocus == 'sidebar_recent)
+	@elseif($currentfocus == 'sidebar_recent)
 		@include('layouts.chatrecently')
 	@endif
 		
