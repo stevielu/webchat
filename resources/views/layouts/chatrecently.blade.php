b3f5d9cfb6229dc2e10758285ee958632c836a3f
stevie <stevie@tecworldnz.com>
2016-07-13 01:47:36 +1200

    weqqwe

--- resources/views/layouts/chatrecently.blade.php
+++ resources/views/layouts/chatrecently.blade.php
@@ -15,8 +15,8 @@
 		<div class="media-left"></div>
 		<div class="media-body">
 			<p class="media-heading" aria-hidden="true">
-				<img class="img-circle" alt="64x64" src="{{$user[$id]->avatar}}" data-holder-rendered="true" style="width: 50px; height: 50px;">
-				{{$user[$id]->name}}
+				<img class="img-circle" alt="64x64" src="{{$user->avatar}}" data-holder-rendered="true" style="width: 50px; height: 50px;">
+				{{$user->name}}
 				<sapn class="pull-right">
 					<span id="noti-cont" class="noti-visible">
 						<span class="cont-badge">0</span>
