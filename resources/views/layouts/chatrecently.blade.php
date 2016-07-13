61caf26d084f4fd3e32f7482521b72e64e17e8b4
stevie <stevie@tecworldnz.com>
2016-07-14 09:10:35 +1200

    dasda

--- resources/views/layouts/chatrecently.blade.php
+++ resources/views/layouts/chatrecently.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="{{asset('css/recent.css')}}"/>
 <div class="add-channel">
 	 <div class="ch-headding">
 	 	<div class="col-md-6 pull-left" style="height: 100%"><p>Recent Contacts</p></div>
@@ -7,22 +8,21 @@
 <div class="search-bar">
 			 <input type="text" class="form-control empty" id="iconified" placeholder="&#xF002; &nbsp search"/>
 </div>
-<ul class="userpannel-lists">
+<ul class="user-lists">
 
 @foreach ($recentContacts as $user)
 <li>	
 	<a href="#">
-		<div class="media-left"></div>
 		<div class="media-body">
-			<p class="media-heading" aria-hidden="true">
+			<span class="media-heading" aria-hidden="true">
 				<img class="img-circle" alt="64x64" src="{{$user['avatar']}}" data-holder-rendered="true" style="width: 50px; height: 50px;">
-				{{$user['name']}}
+				<p>{{$user['name']}}</p>
 				<sapn class="pull-right">
 					<span id="noti-cont" class="noti-visible">
 						<span class="cont-badge">0</span>
 					</span>
 				</sapn>
-			</p>
+			</span>
 		</div>
 	</a>
 </li>
